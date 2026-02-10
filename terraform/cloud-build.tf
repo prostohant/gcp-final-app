145a75bc722ad619e1385e188bc1c2eb06f51b0e
Anton Khlyst <tmxanton.khlyst@kof.com>
2026-02-10 17:22:59 +0200

    app build trigger

--- terraform/cloud-build.tf
+++ terraform/cloud-build.tf
@@ -18,3 +18,22 @@ resource "google_cloudbuild_trigger" "ingestion_trigger" {
   filename = "cloudbuild-ingestor.yaml"
 
 }
+
+resource "google_cloudbuild_trigger" "app_trigger" {
+  name        = "app-dir-trigger"
+  project = var.project_id
+  location      = var.region
+  service_account = module.service_accounts.id
+  description = "Trigger for changes in the app directory"
+
+  github {
+    owner = "prostohant"
+    name  = "gcp-final-app"
+    push {
+      branch = "^main$"
+    }
+  }
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+  included_files = ["app/**"]
+  filename = "cloudbuild-ingestor.yaml"
+}
